ae0be8e39b43bea9fece16393973fb3b11afd05b
Leon-Avalos <heiko.avalos@gmail.com>
2020-09-28 20:21:20 -0500

    [REFACTOR] Changed database by connection and fixed imports

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,11 +3,13 @@ import IndexRoutes from './routes/index.routes'
 import PlantsRoutes from './routes/plants.routes'
 const app = express()
 
-// Configuraciones
+// Settings
 app.set('port', process.env.PORT || 3000)
 app.use(json())
+
+// Routes
 app.use(IndexRoutes)
 app.use(PlantsRoutes)
-// Rutas
+
 
 export default app
